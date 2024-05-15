9381747662b73b1316c9a7a3e9d7c4125a6dcaab
yassirab02 <yassirabourial86@gmail.com>
2024-05-15 13:53:19 +0100

    editing the notfound component

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -2,7 +2,7 @@ export default function NotFound(){
    return (
     <div className="flex flex-col gap-2">
         404 Not Found
-        <Link to="/">Revenir a l'Accueil</Link>
+        <Link to="/accueil">Revenir a l'Accueil</Link>
     </div>
-   )
+   );
 }
